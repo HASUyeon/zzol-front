241863724962ce65acee50032508ebd21f00a16c
hayeonKim <iioo3356@gmail.com>
2025-03-03 04:23:46 +0900

    feat: 회원가입 페이지 추가

--- src/app/oauth/page.tsx
+++ src/app/oauth/page.tsx
@@ -2,9 +2,12 @@
 
 import axios from "axios";
 import { useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 const OAuthPage = () => {
+  const router = useRouter()
+
   const searchParams = useSearchParams();
   console.log(searchParams);
   const code = searchParams.get("code");
@@ -12,8 +15,13 @@ const OAuthPage = () => {
   useEffect(()=>{
 const kakaoLogin = async () => {
   try {
-      const data = await axios.get("http://localhost:8080/auth/sign-in/kakao", {params: {code}})
-console.log(data)
+      const {data} = await axios.get("http://localhost:8080/auth/sign-in/kakao", {params: {code}})
+      console.log(data)
+      const kakaoId = data.kakaoId;
+      const email = data.kakaoAccount.email;
+      if (!data.isRegistered) {
+        router.push(`/sign-up?${new URLSearchParams({kakaoId, email}).toString()}`); // ✅ query를 URL 문자열로 변환
+      }
   }
   catch(err){
     console.log(err)
