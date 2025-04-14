b6d232a9b6c05e39e50b7281f09704dcf39996b3
hayeonKim <iioo3356@gmail.com>
2025-04-15 01:08:00 +0900

    feat: 모달 컴포넌트 원형 추가

--- src/app/game/choice/page.tsx
+++ src/app/game/choice/page.tsx
@@ -1,8 +1,26 @@
+"use client";
+
+import { Modal } from "@/components/Modal/Modal";
+import { useState } from "react";
+
 const GameChoicePage = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleOnOpenRoomMakeModal = () => {
+    setOpen(true);
+  };
+
+  const handleOnCloseRoomMakeModal = () => {
+    setOpen(false);
+  };
+
   return (
     <div>
-      <button>방 만들기</button>
+      <button onClick={handleOnOpenRoomMakeModal}>방 만들기</button>
       <button disabled>랜덤 대전</button>
+      <Modal open={open} onClose={handleOnCloseRoomMakeModal}>
+        방 만들기 모달
+      </Modal>
     </div>
   );
 };
