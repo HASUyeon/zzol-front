f2a1f92b1a39b1311c0edf7a91e748aa42a39200
hayeonKim <iioo3356@gmail.com>
2025-04-17 01:35:41 +0900

    fix: 방 만들기 모달 내용 추가

--- src/app/game/choice/page.tsx
+++ src/app/game/choice/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { Form } from "@/components/Form";
+import { FormItem } from "@/components/FormItem";
 import { Modal } from "@/components/Modal/Modal";
+import clsx from "clsx";
 import { useState } from "react";
 
 const GameChoicePage = () => {
@@ -15,11 +18,47 @@ const GameChoicePage = () => {
   };
 
   return (
-    <div>
-      <button onClick={handleOnOpenRoomMakeModal}>방 만들기</button>
-      <button disabled>랜덤 대전</button>
+    <div className={clsx("h-full", "flex flex-col justify-center")}>
+      <div className={clsx("flex flex-col gap-[16px]")}>
+        <button
+          className={clsx("bg-slate-400 text-slate-50")}
+          onClick={handleOnOpenRoomMakeModal}
+        >
+          방 만들기
+        </button>
+        <button className={clsx("cursor-not-allowed", "bg-slate-100")} disabled>
+          랜덤 대전
+        </button>
+      </div>
       <Modal open={open} onClose={handleOnCloseRoomMakeModal}>
-        방 만들기 모달
+        <Form>
+          <FormItem name="roomName" label="방 제목">
+            <input placeholder="방 제목을 입력해 주세요." />
+          </FormItem>
+          <FormItem name="gameMode" label="게임 모드">
+            <select>
+              <option>가위바위보</option>
+              <option>가위바위보 하나 빼기</option>
+            </select>
+          </FormItem>
+          <FormItem name="betMode" label="내기 모드">
+            <select>
+              <option>다함께</option>
+              <option>따로따로</option>
+            </select>
+          </FormItem>
+          <div className={clsx("flex flex-row gap-[8px]")}>
+            <button
+              className={clsx("w-[100%]", "bg-slate-100")}
+              onClick={handleOnCloseRoomMakeModal}
+            >
+              취소
+            </button>
+            <button className={clsx("w-[100%]", "bg-slate-400 text-slate-50")}>
+              시작하기
+            </button>
+          </div>
+        </Form>
       </Modal>
     </div>
   );
